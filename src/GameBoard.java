1c8edbf0b9514ebc784927e9d27b9d52530efc48
Tharoxes <tharoxes@hotmail.com>
2019-10-03 23:43:26 +0200

    progess001

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -1,27 +1,56 @@
 import java.util.Scanner;
+import java.util.Random;
+import java.lang.Math;
 
 public class GameBoard {
 
     public static void main(String[] args){
 
+        //Input -> int boardSize = ;
+
         //how many players 2-4
-        Scanner numberObject = new Scanner(Sysem.in);
+        Scanner numberObject = new Scanner(System.in);
         System.out.println("Please enter the number of Players: \n");
         int numberOfPlayers = numberObject.nextInt();
 
+        //Input -> What´s their name? e.g. <Player 1>
 
-        //What´s their name? e.g. <Player 1>
 
-        //square image [1][2->6][3][4][5][6][7->9][8][9][10][11->5][12]
 
-        //initial state with players on the first square
+
+        /*
+        square image [1][2->6][3][4][5][6][7->9][8][9][10][11->5][..][boardSize]
+        randomized # Math.round(boardSize/10)snakes and # Math.round(boardSize/5)ladders
+         */
+
+        //print initial state with board and players on the first square
+        //String /stateBefore = initial state
 
         //while game not over
 
-        //one player rolls dice
+            // String stateBefore
 
+            //one player rolls dice
+
+            //print state this.player + " rolls " + this.dice + stateBefore
     }
-    //move the player (position+dice number)
+            //move the player (position+dice number)
+
+            /*if(this.player.position == square.squarenr && square.occupied == 1){
+                    this.player.position=1;}
+             */
+
+            // updatedState = state with moved player
+
+            /*if (this.player.position == boardSize){
+            System.out.println("Final state: " + updatedState \n this.player + "wins!);
+            break;
+            }
+
+             */
+
+
+
     public static void movePlayer() { //make sure to define methods outside the main method
 
         //if
--- src/square.java
+++ src/square.java
@@ -1,4 +1,7 @@
 public class square {
     int squarenr;
     int occupied;
+    //String ladder = "->";
+    //String snake = "<-";
 }
+    //subclasses: snake, ladder, start square, end square
