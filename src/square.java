3646a744ec78e47176b5734f7df39f35365194ed
Christian Aeberhard <gimli51@hotmail.com>
2019-10-04 13:07:56 +0200

    Created Sub-Classes for the Square Superclass - No need to create StartField and EndField Sub-classes, as the Superclass holds the position of a square and we can check with this position if a square is End or Start

--- src/square.java
+++ src/square.java
@@ -1,7 +1,51 @@
-public class square {
-    int squarenr;
-    int occupied;
+//subclasses: snake, ladder
+
+class square {
+    public int squareNr; //placement position on board
+    public int occupied; //0 = free 1 = occupied
+
+    public square(int squareNr, int occupied) {
+        this.squareNr = squareNr;
+        this.occupied = occupied;
+    }
+
     //String ladder = "->";
     //String snake = "<-";
+    public void printSquare(){
+        System.out.println("[" + this.squareNr + "]");
+    }
 }
-    //subclasses: snake, ladder, start square, end square
+
+
+class SnakeSquare extends square{
+    public int backNr; // holds an integer saying how much a player has to move back
+    public SnakeSquare(int squareNr, int occupied, int leap){
+        super(squareNr,occupied);
+        backNr = leap;
+    }
+    @Override
+    public void printSquare(){
+        //System.out.println("[" + this.squareNr-this.backNr +"<-"+ this.squareNr + "]");
+    }
+
+    public void setBackNr(int val){
+        backNr = val;
+    }
+}
+
+
+class LadderSquare extends square{
+    public int advanceNr; // holds an integer saying how much a player has to move back
+    public LadderSquare(int squareNr, int occupied, int leap){
+        super(squareNr,occupied);
+        advanceNr = leap;
+    }
+    @Override
+    public void printSquare(){
+        //System.out.println("[" + this.squareNr +"->"+ this.squareNr+this.advanceNr + "]");
+    }
+
+    public void setBackNr(int val){
+        advanceNr = val;
+    }
+}
