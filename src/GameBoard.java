770a5f412ccffe3c4801fc551badd4f882708340
Tharoxes <tharoxes@hotmail.com>
2019-10-05 11:47:20 +0200

    Backstep of the winning square

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -15,7 +15,7 @@ public class GameBoard {                    /*todo IMPORTANT: in the methods and
 
         int boardSize = setBoardSize(); //Input -> int boardSize = ;
 
-        //Datastructure array with length boardSize and each cell is a square  
+        //Datastructure array with length boardSize and each cell is a square
         square[] table = boardsquares(boardSize);
 
 
@@ -23,62 +23,75 @@ public class GameBoard {                    /*todo IMPORTANT: in the methods and
         square image [1][2->6][3][4][5][6][7->9][8][9][10][11->5][..][boardSize]
         randomized # Math.round(boardSize/10)snakes and # Math.round(boardSize/5)ladders
          */
-                                                                            //todo method placing snakes and ladders
-        //print initial state with board and players on the first square
-        //String /stateBefore = initial state
+        int nrOfSnakes = boardSize/7;
+        int nrOfLadders = boardSize/11;
 
+
+                                                                            //todo method placing snakes and ladders
+
+
+
+
+
+
+        //print initial state with board and players on the first square
         //initial state: players on the first square
 
+        System.out.print("Initial State: ");
+        stateOfTurn(p, boardSize, playerNames);
 
+
+
+        int m = 0;
         while(true) {                  //while game not over
-            int m=0;
-            player playerNow = playerNames[m%p];
 
-            //one player rolls dice
-            int dice = randInt(1, 6);
+            player playerNow = playerNames[m%3];
 
-            //print state this.player + " rolls " + this.dice + stateBefore
+            int backstep;
+            int dice = randInt(1, 6);                               //one player rolls dice
 
-            System.out.print(playerNow + " rolls " + dice + ": ");
 
-            for (int i = 1; i <= boardSize; i++) {
+            System.out.print(playerNow + " rolls " + dice + ": "); //print state this.player + " rolls " + this.dice + stateBefore
 
-                        if (i == 1) {
-                    System.out.print("[" + i);
+            stateOfTurn(p, boardSize, playerNames);
 
-                            for (int a = 0; a < p; a++) {
-                                if(playerNames[a].getPosition()== i) {
 
-                            System.out.print("<" + playerNames[a].getName() + ">");
-                        }
-                    }
-                    System.out.println("]");
-                }
-            }
-
-            for(int a=0, a < boardSize, a+1){
-                if(table[a].getSquareNr() == playerNow.getPosition()){
+            for(int i = 0; i< boardSize; i++){                              //boardgame
+                if(table[i].getSquareNr() == playerNow.getPosition()){
                     square.setOccupied(0);
                     break;
                 }
             }
 
-            if(playerNow.getPosition() + dice == square.getSquareNr() && square.getOccupied() == 1){ //already occupied
-                    playerNow.setPosition(1);
-                    }
-                else{
-                    playerNow.setPosition(playerNow.getPosition()+dice);}         //move the player (position+dice number)
+            if(playerNow.getPosition() + dice >boardSize){
+                backstep =  boardSize-(playerNow.getPosition()+dice-boardSize);
+            }
+            else{
+                backstep = playerNow.getPosition() + dice;
+            }
 
-            // updatedState = state with moved player
+
+            if(backstep == square.getSquareNr() && square.getOccupied() == 1){ //already occupied
+                    playerNow.setPosition(1);
+                    } else{
+                    playerNow.setPosition(backstep);}         //move the player (position+dice number)
+
+
+
+            if(playerNow.getPosition()!=1){
+                table[playerNow.getPosition()-1].setOccupied(1);        //
+            }
+
+
 
             if (playerNow.getPosition() == boardSize){
-                System.out.println("Final state: " + updatedState "\n" + playerNow. + "wins!");
+                System.out.println("Final state: ");
+                stateOfTurn(p, boardSize, playerNames);            // updatedState = state with moved player
+                System.out.println(playerNow.getName() + " wins!");
                 break;
                 }
             m++;
             }
-
-
         }
 
     public static int setPlayerNumber(){
@@ -143,7 +156,7 @@ public class GameBoard {                    /*todo IMPORTANT: in the methods and
     public static square[] boardsquares(int p) {
         square[] boardGame = new square[p]; //initialize String array of size p
         for (int i = 1; i <= p; i++) {
-            boardGame[i] = new square(i+1, 0);
+            boardGame[i-1] = new square(i, 0);
         }
         return boardGame;
     }
@@ -157,6 +170,23 @@ public class GameBoard {                    /*todo IMPORTANT: in the methods and
         }
         Random r = new Random();
         return r.nextInt((max - min) + 1) + min;
+    }
+
+    public static void stateOfTurn(int p, int boardSize, player playerNames[]){
+        for (int i = 1; i <= boardSize; i++) {
+
+            if (i == 1) {
+                System.out.print("[" + i);
+
+                for (int a = 0; a < p; a++) {
+                    if(playerNames[a].getPosition()== i) {
+
+                        System.out.print("<" + playerNames[a].getName() + ">");
+                    }
+                }
+                System.out.println("]");
+            }
+        }
     }
 
     /*public static void movePlayer(int oldpostion, int dice, player) { //make sure to define methods outside the main method
