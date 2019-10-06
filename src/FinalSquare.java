98014914e99928bef311b77fa9a426a78dbce1b2
Filip Dombos <filip.dombos@gmail.com>
2019-10-06 15:09:35 +0200

    Capitalized class names in master branch

--- src/FinalSquare.java
+++ src/FinalSquare.java
@@ -1,4 +1,4 @@
 package PACKAGE_NAME;
 
-public class final_square {
+public class FinalSquare {
 }
--- src/FirstSquare.java
+++ src/FirstSquare.java
@@ -1,4 +1,4 @@
 package PACKAGE_NAME;
 
-public class first_square {
+public class FirstSquare {
 }
