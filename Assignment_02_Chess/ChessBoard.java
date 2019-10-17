fd2ae56b58d758293c2bd86af380a4aa207f45d1
ChristianAeberhard <christian.aeberhard2@uzh.ch>
2019-10-17 09:27:01 +0200

    Initial Chessboard Output Frame ready - Missing implementation of Pieces (please create Piece class @FD)

--- Assignment_02_Chess/ChessBoard.java
+++ Assignment_02_Chess/ChessBoard.java
@@ -3,6 +3,10 @@ public class ChessBoard {
     private Pieces[] pieceSets; //Array for storing PieceSets of Players (Black Set or White Set)
     private PieceColor pieceSetOnTop; //Which color plays on the Top Side of the Board
 
+    public ChessBoard(PieceColor color){
+        this.pieceSetOnTop = color;
+    }
+
     public void create(){ //creates initial chess field and places Player 1 on top of the field
         for(int i = 0; i < squares.length; i++){
             for(int j = 0; j < squares.length; j++){
@@ -20,6 +24,28 @@ public class ChessBoard {
                 System.out.println("Cannot setup Board, unspecified Player Color");
                 break;
         }
+    }
+
+    public void display(){//first Piece Class has to be specified in order to place Pieces
+        System.out.println("\ta\tb\tc\td\te\tf\tg\th");
+        System.out.println("  +---+---+---+---+---+---+---+---+");
+        System.out.println("8 |   |   |   |   |   |   |   |   | 8");
+        System.out.println("  +---+---+---+---+---+---+---+---+");
+        System.out.println("7 |   |   |   |   |   |   |   |   | 7");
+        System.out.println("  +---+---+---+---+---+---+---+---+");
+        System.out.println("6 |   |   |   |   |   |   |   |   | 6");
+        System.out.println("  +---+---+---+---+---+---+---+---+");
+        System.out.println("5 |   |   |   |   |   |   |   |   | 5");
+        System.out.println("  +---+---+---+---+---+---+---+---+");
+        System.out.println("4 |   |   |   |   |   |   |   |   | 4");
+        System.out.println("  +---+---+---+---+---+---+---+---+");
+        System.out.println("3 |   |   |   |   |   |   |   |   | 3");
+        System.out.println("  +---+---+---+---+---+---+---+---+");
+        System.out.println("2 |   |   |   |   |   |   |   |   | 2");
+        System.out.println("  +---+---+---+---+---+---+---+---+");
+        System.out.println("1 |   |   |   |   |   |   |   |   | 1");
+        System.out.println("  +---+---+---+---+---+---+---+---+");
+        System.out.println("\ta\tb\tc\td\te\tf\tg\th");
     }
 }
 
