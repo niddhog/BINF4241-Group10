2562f1838d8fd69da638d1563f15b33f2e0c5ef9
ChristianAeberhard <christian.aeberhard2@uzh.ch>
2019-11-04 10:38:52 +0100

    Implemented an Observer Pattern. Observable: Move Class, observers: each square on the board

--- Assignment_03_DesignPatterns/src/Move.java
+++ Assignment_03_DesignPatterns/src/Move.java
@@ -5,13 +5,6 @@ import java.util.ArrayList;
 public class Move implements Subject {
     private List<Square> observers = new ArrayList<>();
 
-    /*public static void move(Board gameBoard, Coordinates s, Coordinates e) {
-        Piece movedPiece = gameBoard.getBoard()[s.getX()][s.getY()].getPieceOnSquare();
-        gameBoard.getBoard()[s.getX()][s.getY()].deletePieceOnSquare();
-        gameBoard.getBoard()[e.getX()][e.getY()].setPieceOnSquare(movedPiece);
-        movedPiece.setMoved();
-    }*/
-
     public void move(Board gameBoard, Coordinates s, Coordinates e) {
         Piece movedPiece = gameBoard.getBoard()[s.getX()][s.getY()].getPieceOnSquare();
         notifyObserver(s.getX(), s.getY(), e.getX(), e.getY(), movedPiece);
